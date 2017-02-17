1af8196e51bf15cfef90de0ef1cc9ea6f8b10df6
angus <angusdippenaar@gmail.com>
2017-02-17 08:46:24 +0200

    Some more suitable test cases

--- test_emoji_replace.js
+++ test_emoji_replace.js
@@ -4,10 +4,10 @@
 
 const replaceEmoji = require('./emoji_replace');
 
-console.log(replaceEmoji('I need to switch to atleast Capitec Bank. FNB Charges sbali 💔 ngeke!'));
-console.log(replaceEmoji('That sms from capitec 😍😍😍'));
-console.log(replaceEmoji('😁😂😁Lol andithi nani kule App niyasinyisa ngosela Tap water😂😂, by using capitec bank... https://t.co/BrbjFG9YPa'));
-console.log(replaceEmoji('They say tap water is for Capitec clients 😑 Such disrespect 😂😂😂 https://t.co/9BdVBM4Kei'));
+console.log(replaceEmoji('Broke up with my BF 💔'));
+console.log(replaceEmoji('😍😍😍'));
+console.log(replaceEmoji('😁😂😁Lol😂😂. Those socks are amazing'));
+console.log(replaceEmoji('I hate my life 😑 Such disrespect 😂😂😂'));
 console.log(replaceEmoji('asda1⃣asdasd'));
 console.log(replaceEmoji('0⃣1⃣2⃣3⃣4⃣5⃣6⃣7⃣8⃣9⃣🔟#⃣*⃣‼️yes'));
 console.log(replaceEmoji('0 ️ ⃣'));
